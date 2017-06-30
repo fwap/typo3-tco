f814b16514a0bfd2eb498b06aecc5c554cf02e1c
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2017-06-30 14:39:53 +0200

    [FEATURE] Enable Image Cropping

--- src/Image.php
+++ src/Image.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace TildBJ\Tco;
 
@@ -21,6 +21,26 @@ final class Image
     private $maxitems = null;
 
     /**
+     * @var array $cropVariants
+     */
+    private $cropVariants = [];
+
+    /**
+     * @var string $showItems
+     */
+    private $showItems = 'title, alternative';
+
+    /**
+     * @var string $filePalette
+     */
+    private $filePalette = '--palette--;;filePalette';
+
+    /**
+     * @var bool $disableDefaultCropVariant
+     */
+    private $disableDefaultCropVariant = false;
+
+    /**
      * @param $key
      */
     public function __construct(string $key)
@@ -33,6 +53,12 @@ final class Image
      */
     public function toArray(): array
     {
+        $cropVariants['default'] = [
+            'disabled' => $this->disableDefaultCropVariant
+        ];
+        foreach ($this->cropVariants as $key => $cropVariant) {
+            $cropVariants[$key] = $cropVariant;
+        }
         $tca = [
             'exclude' => 1,
             'label' => $this->key,
@@ -44,11 +70,18 @@ final class Image
                     ],
                     'maxitems' => $this->maxitems,
                     'overrideChildTca' => [
+                        'columns' => [
+                            'crop' => [
+                                'config' => [
+                                    'cropVariants' => $cropVariants,
+                                ],
+                            ],
+                        ],
                         'types' => [
                             2 => [
-                                'showitem' => 'title, alternative, --palette--;;filePalette'
-                            ]
-                        ]
+                                'showitem' => $this->showItems . ', ' . $this->filePalette,
+                            ],
+                        ],
                     ],
                 ],
                 $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
@@ -62,9 +95,56 @@ final class Image
      * @param int $max
      * @return Image
      */
-    public function setMaxItems(int $max) : self
+    public function setMaxItems(int $max): self
     {
         $this->maxitems = $max;
+
+        return $this;
+    }
+
+    /**
+     * @return Image
+     */
+    public function enableCropping(): self
+    {
+        $items = explode(',', $this->showItems);
+        if (!in_array('--linebreak--, crop', $items)) {
+            $items[] = '--linebreak--, crop';
+        }
+
+        $this->showItems = implode(',', $items);
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     * @param string $label
+     * @param int $x
+     * @param int $y
+     * @return Image
+     */
+    public function addCropVariant(string $key, string $label, int $x, int $y): self
+    {
+        $this->cropVariants[$key] = [
+            'title' => $label,
+            'allowedAspectRatios' => [
+                $x . ':' . $y => [
+                    'title' => $x . ' x ' . $y,
+                    'value' => $x / $y
+                ]
+            ],
+        ];
+
+        return $this;
+    }
+
+    /**
+     * @return Image
+     */
+    public function disableDefaultCropVariant(): self
+    {
+        $this->disableDefaultCropVariant = true;
 
         return $this;
     }
--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace TildBJ\Tco\Test;
 
@@ -28,5 +28,71 @@ final class ImageTest extends \PHPUnit\Framework\TestCase
     {
         $tca = (new Image('foobar'))->setMaxItems(7)->toArray();
         $this->assertSame(7, $tca['config']['maxitems']);
+    }
+
+    /**
+     * @test
+     */
+    public function enableCropping()
+    {
+        $tco = (new Image('foobar'))->enableCropping();
+        $tca = $tco->toArray();
+
+        $items = explode(',', $tca['config']['overrideChildTca']['types'][2]['showitem']);
+        $this->assertTrue(in_array('crop', $items));
+    }
+
+    /**
+     * @test
+     */
+    public function disableDefaultCropVariant()
+    {
+        $tco = (new Image('foobar'));
+        $tca = $tco->toArray();
+        $this->assertFalse(
+            $tca['config']['overrideChildTca']['columns']['crop']['config']['cropVariants']['default']['disabled']
+        );
+
+        $tco->disableDefaultCropVariant();
+        $tca = $tco->toArray();
+        $this->assertTrue(
+            $tca['config']['overrideChildTca']['columns']['crop']['config']['cropVariants']['default']['disabled']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addCropVariant()
+    {
+        $tca = (new Image('foobar'))
+            ->addCropVariant('foo', 'Foo', 1920, 1080)
+            ->addCropVariant('bar', 'Bar', 1024, 768)
+            ->toArray();
+
+        $expected = [
+            'default' => [
+                'disabled' => false,
+            ],
+            'foo' => [
+                'title' => 'Foo',
+                'allowedAspectRatios' => [
+                    '1920:1080' => [
+                        'title' => '1920 x 1080',
+                        'value' => 1920 / 1080
+                    ],
+                ],
+            ],
+            'bar' => [
+                'title' => 'Bar',
+                'allowedAspectRatios' => [
+                    '1024:768' => [
+                        'title' => '1024 x 768',
+                        'value' => 1024 / 768
+                    ],
+                ],
+            ],
+        ];
+        $this->assertSame($expected, $tca['config']['overrideChildTca']['columns']['crop']['config']['cropVariants']);
     }
 }
